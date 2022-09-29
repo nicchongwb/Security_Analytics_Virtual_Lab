78c5bf2407253aa584a38f6f859d734211800a9b
Yang Jun <2000572@sit.singaporetech.edu.sg>
2022-09-30 00:37:50 +0800

    Added installation of ELK stack
    - Also added configs for Elasticsearch and Kibana

---
+++ ELK Stack/elk_bootstrap.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND="noninteractive"
+
+# Initialize version of ELK stack
+ELK_VERSION="8.4.2"
+
+# Install necessary packages
+echo "[*] Installing necessary packages..."
+sudo apt-get -y install apt-transport-https default-jre &> /dev/null
+echo "[+] Installation of packages completed"
+
+# Import Elastic PGP key and repository
+echo "[*] Importing Elastic PGP key..."
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - &> /dev/null
+echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list &> /dev/null
+sudo apt update &> /dev/null
+echo "[+] Elastic PGP key has been imported"
+
+# Install Elasticsearch
+echo "[*] Installing Elasticsearch..."
+sudo apt-get -y install elasticsearch=$ELK_VERSION &> /dev/null
+echo "[+] Installation of Elasticsearch completed"
+
+# Copy Elasticsearch config and restart daemon
+echo "[*] Copying Elasticsearch config and restarting daemon..."
+cp /vagrant/configs/elasticsearch/elasticsearch.yml /etc/elasticsearch/ &> /dev/null
+sudo /bin/systemctl daemon-reload &> /dev/null
+sudo /bin/systemctl enable elasticsearch.service &> /dev/null
+sudo /bin/systemctl start elasticsearch.service &> /dev/null
+echo "[+] Elasticsearch service started"
+
+# Install Logstash
+echo "[*] Installing Logstash..."
+sudo apt-get -y install logstash=1:$ELK_VERSION-1 &> /dev/null
+echo "[+] Installation of Logstash completed"
+
+# Copy Logstash config and restart daemon
+echo "[*] Copying Logstash config and restarting daemon..."
+cp -R /vagrant/configs/logstash/* /etc/logstash/conf.d/ &> /dev/null
+sudo /bin/systemctl enable logstash.service &> /dev/null
+sudo /bin/systemctl start logstash.service &> /dev/null
+echo "[+] Logstash service started"
+
+# Install Kibana
+echo "[*] Installing Kibana..."
+sudo apt-get install -y kibana=$ELK_VERSION &> /dev/null
+echo "[+] Installation of Kibana completed"
+
+# Copy Kibana config and restart daemon
+echo "[*] Copying Kibana config and restarting daemon..."
+cp /vagrant/configs/kibana/kibana.yml /etc/kibana/ &> /dev/null
+sudo /bin/systemctl daemon-reload &> /dev/null
+sudo /bin/systemctl enable kibana.service &> /dev/null
+sudo /bin/systemctl start kibana.service &> /dev/null
+echo "[+] Kibana service started"
+
+# Echo IP address of machine
+IP_ADDRESS=$(/sbin/ip -o -4 addr list eth1 | awk '{print $4}' | cut -d/ -f1)
+echo "IP address of machine: $IP_ADDRESS"
+echo "Navigate to http://$IP_ADDRESS:9200 to access Elasticsearch!"
+echo "Navigate to http://$IP_ADDRESS:5601 to access Kibana!"
