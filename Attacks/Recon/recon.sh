87a9ffc8584102ddf1e3ee465840e81d444730d6
zhiyutan <73781800+TanZhiYu98@users.noreply.github.com>
2022-10-24 04:35:14 +0800

    removed enum4linux, add port scan

--- Attacks/Recon/recon.sh
+++ Attacks/Recon/recon.sh
@@ -6,10 +6,10 @@ nmap -e tun0 -sn 10.0.0.0/8 172.16-31.0.0/16 192.168.0.0/16 -oG - | awk '/Up$/{p
 echo "The following hosts are up..."
 cat results
 
-echo "---- enum4linux ----"
+echo "---- NMAP port scan ----"
 while read line
 do
-echo "Running enum4linux for {$line}"
-enum4linux -a $line
+echo "Running port scan for {$line}"
+nmap -A $line
 done < results
 
